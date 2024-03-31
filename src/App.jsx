ae83de65d78a400bf68846fc3e23687b53628593
Chinmay-Ankolekar <chinmaysankolekar@gmail.com>
2024-03-31 11:42:07 +0530

    created landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import LandingPage from './components/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-      </div>
-      
+        <LandingPage />
     </>
   )
 }
